12ad3d87f1d591dd026a479720f836b5ed11ea57
Ciclo <ciclointegrado@gmail.com>
2026-02-18 00:30:19 -0300

    chore: remover arquivos temporarios de seed

--- _run-sec.js
+++
@@ -1,8 +0,0 @@
-const { spawnSync } = require('child_process');
-const r = spawnSync('node', [
-  'api/scripts/seed-secretarias.js',
-  'https://www.transpjardim.com',
-  'franciscodesenvolve',
-  'Platao3914$Mouse'
-], { stdio: 'inherit', shell: false });
-process.exit(r.status || 0);
